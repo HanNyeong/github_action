47b75b7ab248d34e84f91a4106c6ba4a75751593
Noah Lee <lhn4110@gmail.com>
2021-10-17 16:25:49 +0900

    # 제목의 길이는 최대 40글자까지 한글로 간단 명료하게 작성 # 제목을 작성하고 반드시 빈 줄 한 줄을 만들어야 함 # 제목에 .(마침표) 금지 # <label> 리스트 # :sparkles: - 새로운 기능 추가 ✨ # :bug: - 버그 수정 🐛 # :recycle: - 소스 리펙토링 ♻️ # :test_tube: - 테스트 코드 추가 🧪
    # :lipstick: - UI 추가 및 수정 💄
    # :necktie: - 기획 변경으로 인한 수정
    # :chart_with_upwards_trend: - 마켓팅 코드 관련 추가 및 수정
    # :wrench: - 설정 파일 추가 및 수정
    # :see_no_evil: - .gitgnore file 수정 🙈
    
    # :memo: - 문서 추가 및 수정 📝
    # :truck: - 파일, 디렉토리 변경 🚚
    # :fire: - 불 필요한 코드 및 파일 제거 🔥
    
    # :bulb: - 소스코드 코멘트 💡
    # 외의 추가 항목 필요한 경우 (https://gitmoji.dev/)를 참고하여 Jaden << 에게 문의주세요.
    # 내용의 길이는 한 줄당 60글자 내외에서 줄 바꿈. 한글로 간단 명료하게 작성
    # 어떻게 보다는 무엇을, 왜 변경했는지를 작성할 것 (필수)
    
    # 연관된 이슈 첨부, 여러 개 추가 가능

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # github_action
+
 깃허브 액션 사용해보기
---
+++ src/test/java/com/noah/github_action/GithubActionApplicationTests.java
@@ -0,0 +1,24 @@
+package com.noah.github_action;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class GithubActionApplicationTests {
+
+
+	@Test
+	@DisplayName("Hello My Test1")
+	void contextLoads() {
+		assertEquals(2, 1+1);
+	}
+
+	@Test
+	@DisplayName("Hello My Test2")
+	void failTest() {
+		assertEquals(2, 1+3);
+	}
+
+}
